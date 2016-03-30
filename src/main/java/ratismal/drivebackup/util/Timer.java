dfe49d813585ec73fb2be0fc922554b148051b6a
ratismal <kirkbenm@gmail.com>
2016-03-30 09:32:49 -0600

    timer docs

--- src/main/java/ratismal/drivebackup/util/Timer.java
+++ src/main/java/ratismal/drivebackup/util/Timer.java
@@ -16,11 +16,18 @@ public class Timer {
 
     }
 
+    /**
+     * Starts the timer
+     */
     public void start() {
         start = new Date();
         end = null;
     }
 
+    /**
+     * Ends the timer
+     * @return false if timer was never started
+     */
     public boolean end() {
         if (start == null) {
             return false;
@@ -29,6 +36,11 @@ public class Timer {
         return true;
     }
 
+    /**
+     * Construct an upload message
+     * @param file file that was uploaded
+     * @return message
+     */
     public String getUploadTimeMessage(File file) {
         DecimalFormat df = new DecimalFormat("#.##");
         double difference = getTime();
@@ -38,6 +50,10 @@ public class Timer {
         return "File uploaded in " + length + " seconds (" + speed + "KB/s)";
     }
 
+    /**
+     * Calculates the time
+     * @return Calculated time
+     */
     public double getTime() {
         return end.getTime() - start.getTime();
     }
