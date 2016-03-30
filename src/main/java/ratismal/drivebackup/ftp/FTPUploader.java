7523e6abd9b63c5eb19a1b89ba0f079ea8f691ee
ratismal <kirkbenm@gmail.com>
2016-03-30 10:51:35 -0600

    implemented ftp deletes

--- src/main/java/ratismal/drivebackup/ftp/FTPUploader.java
+++ src/main/java/ratismal/drivebackup/ftp/FTPUploader.java
@@ -9,6 +9,9 @@ import ratismal.drivebackup.util.MessageUtil;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Ratismal on 2016-03-30.
@@ -45,9 +48,11 @@ public class FTPUploader {
 
             f.setFileType(FTP.BINARY_FILE_TYPE, FTP.BINARY_FILE_TYPE);
             f.setFileTransferMode(FTP.BINARY_FILE_TYPE);
+            f.setListHiddenFiles(false);
 
             FileInputStream fs = new FileInputStream(file);
             f.storeFile(file.getName(), fs);
+            fs.close();
 
             MessageUtil.sendConsoleMessage(f.printWorkingDirectory());
 
@@ -60,8 +65,30 @@ public class FTPUploader {
     }
 
     public static void deleteFiles(FTPClient f, String type) throws Exception {
-        FTPFile[] files = f.listFiles();
+        int fileLimit = Config.getKeepCount();
+        if (fileLimit == -1) {
+            return;
+        }
+        TreeMap<Date, FTPFile> files = processFiles(f);
 
-        if (files)
+        if (files.size() > fileLimit) {
+            MessageUtil.sendConsoleMessage("There are " + files.size() + " file(s) which exceeds the " +
+                    "limit of " + fileLimit + ", deleting.");
+            while (files.size() > fileLimit) {
+                f.deleteFile(files.firstEntry().getValue().getName());
+                files.remove(files.firstKey());
+            }
+        }
     }
+
+    public static TreeMap<Date, FTPFile> processFiles(FTPClient f) throws Exception {
+        TreeMap<Date, FTPFile> result = new TreeMap<Date, FTPFile>();
+        for (FTPFile file : f.mlistDir()) {
+            if (file.getName().endsWith(".zip"))
+            result.put(file.getTimestamp().getTime(), file);
+        }
+        return result;
+    }
+
+
 }
