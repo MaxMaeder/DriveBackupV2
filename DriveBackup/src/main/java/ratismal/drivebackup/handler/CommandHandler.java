d071fd13b6154459d282490348c25b5b0a7903fa
Ralph Drake <rama.o.ralph@gmail.com>
2020-07-11 05:30:16 -0400

    Allow server console to run /drivebackup commands

--- DriveBackup/src/main/java/ratismal/drivebackup/handler/CommandHandler.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/handler/CommandHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitScheduler;
 
@@ -44,8 +45,8 @@ public class CommandHandler implements CommandExecutor {
      * @return whether the command was handled
      */
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            MessageUtil.sendMessage(sender, "DriveBackupV2 only supports commands sent in-game");
+        if (!(sender instanceof Player || sender instanceof ConsoleCommandSender)) {
+            MessageUtil.sendMessage(sender, "Please run DriveBackupV2 through console or in game.");
             return true;
         }
         
