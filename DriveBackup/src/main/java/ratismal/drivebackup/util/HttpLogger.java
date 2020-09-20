04184a333c02ba0859afecfe410cf5e54f8783da
Max Maeder <max.s.maeder@gmail.com>
2020-09-20 10:17:07 -0500

    Added backup compression setting.

--- DriveBackup/src/main/java/ratismal/drivebackup/util/HttpLogger.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/util/HttpLogger.java
@@ -3,10 +3,15 @@ package ratismal.drivebackup.util;
 import java.io.IOException;
 
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
+import okio.Buffer;
 
 public class HttpLogger implements Interceptor {
+  private static final MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
+
   @Override
   public Response intercept(Interceptor.Chain chain) throws IOException {
     Request request = chain.request();
@@ -18,11 +23,26 @@ public class HttpLogger implements Interceptor {
     Response response = chain.proceed(request);
 
     long t2 = System.nanoTime();
-    MessageUtil.sendConsoleMessage(String.format("Received response for %s in %.1fms%n%s", response.request().url(),
-        (t2 - t1) / 1e6d, response.headers()));
+    MessageUtil.sendConsoleMessage(String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
 
-    MessageUtil.sendConsoleMessage(response.body().string());
+    try {
+      if (request.body().contentType().equals(jsonMediaType)) {
+        final Buffer requestBody = new Buffer();
+        request.body().writeTo(requestBody);
+        MessageUtil.sendConsoleMessage("req: " + requestBody.readUtf8());
+      } else {
+        MessageUtil.sendConsoleMessage("req: not JSON");
+      }
+    } catch (Exception exception) {
+      MessageUtil.sendConsoleMessage("req: none");
+    }
 
-    return response;
+    ResponseBody responseBody = response.body();
+    String responseBodyString = responseBody.string();
+    MediaType responseBodyContentType = responseBody.contentType();
+    responseBody.close();
+    MessageUtil.sendConsoleMessage("res: " + responseBodyString);
+
+    return response.newBuilder().body(ResponseBody.create(responseBodyContentType, responseBodyString)).build();
   }
 }
