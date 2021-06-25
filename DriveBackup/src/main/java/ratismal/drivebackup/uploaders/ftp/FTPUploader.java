eac87843af8fa151c4824ba8d3095883880310f2
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-06-25 00:16:12 +0000

    Remember to import classes...

--- DriveBackup/src/main/java/ratismal/drivebackup/uploaders/ftp/FTPUploader.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/ftp/FTPUploader.java
@@ -15,6 +15,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.net.UnknownHostException;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
