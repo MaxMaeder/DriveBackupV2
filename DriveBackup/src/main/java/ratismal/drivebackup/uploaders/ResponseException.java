cb673c242f4263a738c379a7d23974321b173f44
MaxMaeder <max.s.maeder@gmail.com>
2021-07-11 09:30:10 -0500

    Added ResponseException

---
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/ResponseException.java
@@ -0,0 +1,41 @@
+package ratismal.drivebackup.uploaders;
+
+import org.json.JSONObject;
+
+/**
+ * Thrown to indicate an error with the response of a http request
+ */
+class ResponseException extends Exception {
+    JSONObject json;
+
+    /**
+     * Constructs an {@code ResponseException} with the
+     * specified JSON response.
+     *
+     * @param j the JSON response.
+     */
+    public ResponseException(JSONObject j) {
+        super();
+        json = j;
+    }
+
+    /**
+     * Constructs an {@code ResponseException} with the
+     * specified JSON response and detail message.
+     *
+     * @param j the JSON response.
+     * @param s the detail message.
+     */
+    public ResponseException(JSONObject j, String s) {
+        super(s);
+        json = j;
+    }
+
+    /**
+     * Gets the JSON response
+     * @return the JSON response
+     */
+    public JSONObject getJsonObject() {
+        return json;
+    }
+}
