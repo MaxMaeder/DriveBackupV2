a1292a11318f612a87d15ef22781b51d8327da4f
Rhodri <24809571+rhld16@users.noreply.github.com>
2021-07-25 22:32:05 +0000

    Fix rare auth bug

--- Authenticator/routes/callback.js
+++ Authenticator/routes/callback.js
@@ -2,7 +2,7 @@ const { db } = require('../app.js');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function(req, res) {
   if (req.query.state === undefined || req.query.code === undefined) {
     return res.send({
       success: false,
@@ -11,9 +11,8 @@ router.get('/', async function(req, res, next) {
   }
   var docRef = db.collection('pins').doc(req.query.state);
 
-  await docRef.update({
-    auth_code: req.query.code
-  });
+  await docRef.update({ auth_code: req.query.code });
+
   res.sendFile(process.cwd() + '/views/success.html');
 });
 
