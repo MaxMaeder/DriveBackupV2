4f39ebef6647ba81112a099b8e9fb4892bc71cb7
Rhodri <24809571+rhld16@users.noreply.github.com>
2021-08-18 17:44:50 +0000

    Add friendlier fail page and some svg changes

--- Authenticator/dev.js
+++ Authenticator/dev.js
@@ -7,26 +7,7 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.get('/privacy-policy', function(req, res) {
-  res.sendFile(__dirname + '/views/privacy-policy.html');
-});
-
-app.get('/about', function(req, res) {
-  res.sendFile(__dirname + '/views/about.html');
-});
-
-app.get('/callback', function(req, res) {
-  res.sendFile(__dirname + '/views/success.html');
-});
-
-app.get('/drive-selection', function(req, res) {
-  res.sendFile(__dirname + '/views/selector.html');
-});
+app.use(express.static(__dirname + '/views'));
 
 app.listen(8080, () => {
    console.log("App listening");
--- Authenticator/routes/callback.js
+++ Authenticator/routes/callback.js
@@ -3,17 +3,14 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function(req, res) {
-  if (req.query.state === undefined || req.query.code === undefined) {
-    return res.send({
-      success: false,
-      msg: "Code or state not present"
-    })
+  if (req.query.state === undefined || req.query.code === undefined || req.query.error != undefined) {
+    res.sendFile(__dirname + '/views/fail.html');
   }
   var docRef = db.collection('pins').doc(req.query.state);
 
   await docRef.update({ auth_code: req.query.code });
 
-  res.sendFile(process.cwd() + '/views/success.html');
+  res.sendFile(__dirname + '/views/success.html');
 });
 
 module.exports = router;
