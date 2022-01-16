338ca20e7d8130b8f76172d33fa8ec4338ad1e12
Odd Stråbø <oddstr13@openshell.no>
2022-01-16 14:46:08 +0100

    MySQL: Stream data directly to file

--- DriveBackup/src/main/java/ratismal/drivebackup/uploaders/mysql/MySQLUploader.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/mysql/MySQLUploader.java
@@ -5,8 +5,11 @@
 
 package ratismal.drivebackup.uploaders.mysql;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -16,6 +19,7 @@ import java.sql.Statement;
 import java.sql.Types;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -76,23 +80,34 @@ public class MySQLUploader {
      * @param blacklist a list of tables to not include
      */
     public void downloadDatabase(String name, String type, List<String> blacklist) {
+        String connectionUrl = "jdbc:mysql://" + host + ":" + port + "/" + name
+                + "?useUnicode=true"
+                + "&useJDBCCompliantTimezoneShift=true"
+                + "&zeroDateTimeBehavior=convertToNull"
+                + "&useLegacyDatetimeCode=false"
+                + "&serverTimezone=UTC"
+                + "&useSSL=" + useSsl;
         try {
-            String connectionUrl = "jdbc:mysql://" + host + ":" + port + "/" + name + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&zeroDateTimeBehavior=convertToNull&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=" + useSsl;
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(connectionUrl, username, password);
+            try (Connection connection = DriverManager.getConnection(connectionUrl, username, password)) {
+                stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            stmt = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                File outputPath = new java.io.File("external-backups" + File.separator + type);
+                if (!outputPath.exists()) {
+                    outputPath.mkdirs();
+                }
 
-            String sql = getInsertStatements(name, blacklist);
-    
-            File outputFile = new java.io.File("external-backups" + File.separator + type);
-            if (!outputFile.exists()) {
-                outputFile.mkdirs();
+                try (FileOutputStream outputStream = new FileOutputStream(
+                        outputPath + File.separator + name + ".sql")) {
+                    try (BufferedOutputStream _bos = new BufferedOutputStream(outputStream)) {
+                        try (OutputStreamWriter _osw = new OutputStreamWriter(_bos, "UTF-8")) {
+                            getInsertStatements(_osw, name, blacklist);
+                        }
+                    }
+                }
             }
-    
-            FileOutputStream outputStream = new FileOutputStream(outputFile + File.separator + name + ".sql");
-            outputStream.write(sql.getBytes());
-            outputStream.close();
+            
+
         } catch (Exception e) {
             MessageUtil.sendConsoleException(e);
             setErrorOccurred(true);
@@ -121,9 +136,7 @@ public class MySQLUploader {
      * @return the SQL insert statement
      * @throws SQLException
      */
-    private String getTableInsertStatement(String name) throws SQLException {
-
-        StringBuilder sql = new StringBuilder();
+    private void getTableInsertStatement(OutputStreamWriter sql, String name) throws SQLException, IOException {
         ResultSet rs;
 
         rs = stmt.executeQuery("SHOW CREATE TABLE " + "`" + name + "`;");
@@ -141,7 +154,6 @@ public class MySQLUploader {
         sql.append("\n").append(SQL_END_PATTERN).append("  table dump : ").append(name);
         sql.append("\n--\n\n");
 
-        return sql.toString();
     }
 
 
@@ -151,9 +163,7 @@ public class MySQLUploader {
      * @return the SQL insert statement
      * @throws SQLException exception
      */
-    private String getDataInsertStatement(String name) throws SQLException {
-
-        StringBuilder sql = new StringBuilder();
+    private void getDataInsertStatement(OutputStreamWriter sql, String name) throws SQLException, IOException {
 
         ResultSet rs = stmt.executeQuery("SELECT * FROM " + "`" + name + "`;");
 
@@ -161,9 +171,8 @@ public class MySQLUploader {
         rs.last();
         int rowCount = rs.getRow();
 
-        //there are no records just return empty string
         if(rowCount <= 0) {
-            return sql.toString();
+            return;
         }
 
         sql.append("\n--").append("\n-- Inserts of ").append(name).append("\n--\n\n");
@@ -175,7 +184,7 @@ public class MySQLUploader {
                 .append(SQL_START_PATTERN).append(" table insert : ").append(name)
                 .append("\n--\n");
 
-        sql.append("INSERT INTO `").append(name).append("`(");
+        sql.append("INSERT INTO `").append(name).append("` (");
 
         ResultSetMetaData metaData = rs.getMetaData();
         int columnCount = metaData.getColumnCount();
@@ -184,52 +193,81 @@ public class MySQLUploader {
         //in the returned result set
         //at this point the insert is INSERT INTO (`col1`, `col2`, ...)
         for(int i = 0; i < columnCount; i++) {
-            sql.append("`")
-                    .append(metaData.getColumnName( i + 1))
-                    .append("`, ");
+            if (i > 0) {
+                sql.append(", ");
+            }
+            sql.append("`");
+            sql.append(metaData.getColumnName( i + 1));
+            sql.append("`");
         }
 
-        //remove the last whitespace and comma
-        sql.deleteCharAt(sql.length() - 1).deleteCharAt(sql.length() - 1).append(") VALUES \n");
+        sql.append(") VALUES \n");
 
         //now we're going to build the values for data insertion
         rs.beforeFirst();
         while(rs.next()) {
+            if (!rs.isFirst()) {
+                sql.append(",\n");
+            }
             sql.append("(");
             for(int i = 0; i < columnCount; i++) {
 
                 int columnType = metaData.getColumnType(i + 1);
                 int columnIndex = i + 1;
 
-                //this is the part where the values are processed based on their type
-                if(rs.getObject(columnIndex) == null) {
-                    sql.append("").append(rs.getObject(columnIndex)).append(", ");
+                if (i > 0) {
+                    sql.append(", ");
                 }
-                else if( columnType == Types.INTEGER || columnType == Types.TINYINT || columnType == Types.BIT) {
-                    sql.append(rs.getInt(columnIndex)).append(", ");
-                }
-                else {
 
-                    String val = rs.getString(columnIndex);
-                   //escape the single quotes that might be in the value
-                    val = val.replace("'", "\\'");
+                // this is the part where the values are processed based on their type
+                if (rs.getObject(columnIndex) == null) {
+                    sql.append("NULL");
+                } else {
+                    switch (columnType) {
+                        case Types.BIT:
+                        case Types.TINYINT:
+                        case Types.SMALLINT:
+                        case Types.INTEGER:
+                        case Types.BIGINT:
+                            sql.append(Long.toString(rs.getLong(columnIndex)));
+                            break;
+                        
+                        case Types.FLOAT:
+                            sql.append(Float.toString(rs.getFloat(columnIndex)));
+                            break;
+                        case Types.DOUBLE:
+                            sql.append(Double.toString(rs.getDouble(columnIndex)));
+                            break;
+                        
+                        case Types.DECIMAL:
+                            sql.append(rs.getBigDecimal(columnIndex).toString());
+                            break;
 
-                    sql.append("'").append(val).append("', ");
+                        case Types.BINARY:
+                        case Types.VARBINARY:
+                        case Types.LONGVARBINARY:
+                        case Types.BLOB:
+                            // TODO: Replace this with a streaming pipeline
+                            // Possibly org.apache.commons.codec.binary.Base64InputStream
+                            // WARNING: Can cause excessive memory usage!
+                            sql.append("FROM_BASE64('");
+                            sql.append(Base64.getEncoder().encodeToString(rs.getBytes(columnIndex)));
+                            sql.append("')");
+                            break;
+
+                        default:
+                            // TODO: Replace this with a streaming pipeline
+                            // WARNING: Can cause excessive memory usage!
+                            String val = rs.getString(columnIndex);
+                            // escape the single quotes that might be in the value
+                            val = val.replace("'", "\\'");
+                            sql.append("'").append(val).append("'");
+                            break;
+                    }
                 }
             }
 
-            //now that we're done with a row
-            //let's remove the last whitespace and comma
-            sql.deleteCharAt(sql.length() - 1).deleteCharAt(sql.length() - 1);
-
-            //if this is the last row, just append a closing
-            //parenthesis otherwise append a closing parenthesis and a comma
-            //for the next set of values
-            if(rs.isLast()) {
-                sql.append(")");
-            } else {
-                sql.append("),\n");
-            }
+            sql.append(")");
         }
 
         //now that we are done processing the entire row
@@ -242,8 +280,6 @@ public class MySQLUploader {
 
         //enable FK constraint
         sql.append("\n/*!40000 ALTER TABLE `").append(name).append("` ENABLE KEYS */;\n");
-
-        return sql.toString();
     }
 
     /**
@@ -253,9 +289,7 @@ public class MySQLUploader {
      * @return the SQL insert statement
      * @throws SQLException exception
      */
-    private String getInsertStatements(String name, List<String> blacklist) throws SQLException {
-
-        StringBuilder sql = new StringBuilder();
+    private void getInsertStatements(OutputStreamWriter sql, String name, List<String> blacklist) throws SQLException, IOException {
         sql.append("--");
         sql.append("\n-- Generated by DriveBackupV2");
         sql.append("\n-- http://dev.bukkit.org/projects/drivebackupv2");
@@ -280,8 +314,8 @@ public class MySQLUploader {
             }
 
             try {
-                sql.append(getTableInsertStatement(table.trim()));
-                sql.append(getDataInsertStatement(table.trim()));
+                getTableInsertStatement(sql, table.trim());
+                getDataInsertStatement(sql, table.trim());
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -289,9 +323,7 @@ public class MySQLUploader {
 
         sql.append("\n/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;")
                 .append("\n/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;")
-                .append("\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;");
-
-        return sql.toString();
+                .append("\n/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;\n");
     }
 
     /**
