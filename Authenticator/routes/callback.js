2fd73cfbf7e0a5b4754a744e47ecd550d636aa90
Rhodri <24809571+rhld16@users.noreply.github.com>
2022-03-06 13:53:43 +0000

    [Authenticator] Send fail.html from correct directory

--- Authenticator/routes/callback.js
+++ Authenticator/routes/callback.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async function (req, res) {
-  if (req.query.state === undefined || req.query.code === undefined || req.query.error != undefined) return res.sendFile(__dirname + '/views/fail.html');
+  if (req.query.state === undefined || req.query.code === undefined || req.query.error != undefined) return res.sendFile(process.cwd() + '/views/fail.html');
 
   await db.collection('pins').doc(req.query.state).update({
     auth_code: req.query.code
