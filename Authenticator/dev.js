51132d392af316ee854b787198bfcc0fb02c0b95
rhld16 <24809571+rhld16@users.noreply.github.com>
2023-11-28 17:40:42 +0000

    [Authenticator] Refine Privacy Policy

--- Authenticator/dev.js
+++ Authenticator/dev.js
@@ -7,7 +7,18 @@ app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/views'));
+
+app.get('/', function (req, res) {
+   res.sendFile(__dirname + '/views/index.html');
+ });
+ 
+ app.get('/privacy-policy', function(req, res) {
+   res.sendFile(__dirname + '/views/privacy-policy.html');
+ });
+ 
+ app.get('/about', function(req, res) {
+   res.sendFile(__dirname + '/views/about.html');
+ });
 
 app.listen(8080, () => {
    console.log("App listening");
