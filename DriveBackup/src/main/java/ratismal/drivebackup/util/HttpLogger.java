4cff2222a3af053cb5c90b19e4ea8e81bcadb13a
LRMtheBoss <lucas.mcginnis@lrmtheboss.com>
2024-01-28 13:14:21 -0500

    Reformat file to use 4 space tabs
    add brackets to if statements

--- DriveBackup/src/main/java/ratismal/drivebackup/util/HttpLogger.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/util/HttpLogger.java
@@ -14,50 +14,47 @@ import okio.Buffer;
 import ratismal.drivebackup.config.ConfigParser;
 
 public class HttpLogger implements Interceptor {
-  private static final MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
-
-  @Override
-  public Response intercept(@NotNull Interceptor.Chain chain) throws IOException {
-    Request request = chain.request();
-    if (!ConfigParser.getConfig().advanced.debugEnabled)
-      return chain.proceed(request);
-
-    long t1 = System.nanoTime();
-    MessageUtil.Builder().text(String.format("Sending request %s", request.url())).toConsole(true).send();
-
-    Response response = chain.proceed(request);
-
-    long t2 = System.nanoTime();
-    MessageUtil.Builder().text(String.format("Received response for %s in %.1fms", response.request().url(), (t2 - t1) / 1e6d)).toConsole(true).send();
-
-    try {
-      if (request.body().contentType().equals(jsonMediaType)) {
-        final Buffer requestBody = new Buffer();
-        request.body().writeTo(requestBody);
-        MessageUtil.Builder().text("Req: " + requestBody.readUtf8()).toConsole(true).send();
-      } else {
-        MessageUtil.Builder().text("Req: Not JSON").toConsole(true).send();
-      }
-    } catch (Exception exception) {
-      MessageUtil.Builder().text("Req: None").toConsole(true).send();
+    private static final MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
+    
+    @Override
+    public Response intercept(@NotNull Interceptor.Chain chain) throws IOException {
+        Request request = chain.request();
+        if (!ConfigParser.getConfig().advanced.debugEnabled) {
+            return chain.proceed(request);
+        }
+        long t1 = System.nanoTime();
+        MessageUtil.Builder().text(String.format("Sending request %s", request.url())).toConsole(true).send();
+        Response response = chain.proceed(request);
+        long t2 = System.nanoTime();
+        MessageUtil.Builder().text(String.format("Received response for %s in %.1fms", response.request().url(), (t2 - t1) / 1e6d)).toConsole(true).send();
+        try {
+            if (request.body().contentType().equals(jsonMediaType)) {
+                Buffer requestBody = new Buffer();
+                request.body().writeTo(requestBody);
+                MessageUtil.Builder().text("Req: " + requestBody.readUtf8()).toConsole(true).send();
+            } else {
+                MessageUtil.Builder().text("Req: Not JSON").toConsole(true).send();
+            }
+        } catch (Exception exception) {
+            MessageUtil.Builder().text("Req: None").toConsole(true).send();
+        }
+        ResponseBody responseBody = response.body();
+        String responseBodyString = responseBody.string();
+        MediaType responseBodyContentType = responseBody.contentType();
+        responseBody.close();
+        if (responseBodyContentType.equals(jsonMediaType)) {
+            try {
+                JSONObject responseBodyJson = new JSONObject(responseBodyString);
+                if (responseBodyJson.getString("msg").equals("code_not_authenticated")) {
+                    return response.newBuilder().body(ResponseBody.create(responseBodyString, responseBodyContentType)).build();
+                }
+                MessageUtil.Builder().text("Resp: " + responseBodyJson).toConsole(true).send();
+            } catch (Exception exception) {
+                MessageUtil.Builder().text("Resp: " + responseBodyString).toConsole(true).send();
+            }
+        } else {
+            MessageUtil.Builder().text("Resp: " + responseBodyString).toConsole(true).send();
+        }
+        return response.newBuilder().body(ResponseBody.create(responseBodyString, responseBodyContentType)).build();
     }
-
-    ResponseBody responseBody = response.body();
-    String responseBodyString = responseBody.string();
-    MediaType responseBodyContentType = responseBody.contentType();
-    responseBody.close();
-    if (responseBodyContentType.equals(jsonMediaType)) {
-      try {
-        JSONObject responseBodyJson = new JSONObject(responseBodyString);
-        if (responseBodyJson.getString("msg").equals("code_not_authenticated")) return response.newBuilder().body(ResponseBody.create(responseBodyString, responseBodyContentType)).build();
-        MessageUtil.Builder().text("Resp: " + responseBodyJson).toConsole(true).send();
-      } catch (Exception exception) {
-        MessageUtil.Builder().text("Resp: " + responseBodyString).toConsole(true).send();
-      }
-    } else {
-      MessageUtil.Builder().text("Resp: " + responseBodyString).toConsole(true).send();
-    }
-
-    return response.newBuilder().body(ResponseBody.create(responseBodyString, responseBodyContentType)).build();
-  }
 }
