cc80ea8c1f2c16f0a35b42579185a9690ebcf59f
LRMtheBoss <lucas.mcginnis@lrmtheboss.com>
2024-01-28 13:25:57 -0500

    Removed unnecessary public modifier

--- DriveBackup/src/main/java/ratismal/drivebackup/uploaders/Uploader.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/Uploader.java
@@ -5,12 +5,12 @@ import ratismal.drivebackup.uploaders.Authenticator.AuthenticationProvider;
 import java.io.IOException;
 
 public interface Uploader {
-    public String getName();
-    public String getId();
-    public AuthenticationProvider getAuthProvider();
-    public boolean isAuthenticated();
-    public boolean isErrorWhileUploading();
-    public void test(java.io.File testFile);
-    public void uploadFile(java.io.File file, String type) throws IOException;
-    public void close();
+    String getName();
+    String getId();
+    AuthenticationProvider getAuthProvider();
+    boolean isAuthenticated();
+    boolean isErrorWhileUploading();
+    void test(java.io.File testFile);
+    void uploadFile(java.io.File file, String type) throws IOException;
+    void close();
 }
