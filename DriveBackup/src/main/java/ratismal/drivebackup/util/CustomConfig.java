0868016dda7a1f7dbc29ba45e6f8a5c236c3265c
LRMtheBoss <lucas.mcginnis@lrmtheboss.com>
2024-01-30 13:45:41 -0500

    Switch Charsets to StandardCharsets

--- DriveBackup/src/main/java/ratismal/drivebackup/util/CustomConfig.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/util/CustomConfig.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 
 import com.google.common.base.Charsets;
@@ -29,7 +30,7 @@ public class CustomConfig {
         if (defConfigStream == null) {
             return;
         }
-        config.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8)));
+        config.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, StandardCharsets.UTF_8)));
     }
 
     public FileConfiguration getConfig() {
