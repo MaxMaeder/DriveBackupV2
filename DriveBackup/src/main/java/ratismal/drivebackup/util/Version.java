2a51086941c4d855c90281b18e2d1c6e611d1d82
LRMtheBoss <lucas.mcginnis@lrmtheboss.com>
2024-03-25 12:09:14 -0400

    Added additional version compare methods

--- DriveBackup/src/main/java/ratismal/drivebackup/util/Version.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/util/Version.java
@@ -3,29 +3,30 @@ package ratismal.drivebackup.util;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-public class Version {
+public final class Version {
     private final int major;
     private final int minor;
     private final int patch;
-
+    
+    @Contract (pure = true)
     private Version(int major, int minor, int patch) {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
     }
-
+    
     @NotNull
     @Contract ("_ -> new")
-    public static Version parse(@NotNull String version) throws ArrayIndexOutOfBoundsException, NumberFormatException {
+    public static Version parse(@NotNull String version) throws NumberFormatException {
         String[] splitVersion = version.split("\\.");
-
         return new Version(
-            Integer.parseInt(splitVersion[0]), 
-            Integer.parseInt(splitVersion[1]), 
+            Integer.parseInt(splitVersion[0]),
+            Integer.parseInt(splitVersion[1]),
             Integer.parseInt(splitVersion[2])
         );
     }
-
+    
+    @Deprecated
     public boolean isAfter(@NotNull Version other) {
         if (major != other.major) {
             return major > other.major;
@@ -36,11 +37,40 @@ public class Version {
         if (patch != other.patch) {
             return patch > other.patch;
         }
-
         return false;
     }
-
-    public String toString() {
+    
+    @Contract (pure = true)
+    public boolean isNewerThan(@NotNull Version other) {
+        if (major != other.major) {
+            return major > other.major;
+        }
+        if (minor != other.minor) {
+            return minor > other.minor;
+        }
+        if (patch != other.patch) {
+            return patch > other.patch;
+        }
+        return false;
+    }
+    
+    @Contract (pure = true)
+    public boolean isOlderThan(@NotNull Version other) {
+        if (major != other.major) {
+            return major < other.major;
+        }
+        if (minor != other.minor) {
+            return minor < other.minor;
+        }
+        if (patch != other.patch) {
+            return patch < other.patch;
+        }
+        return false;
+    }
+    
+    @Contract (pure = true)
+    @Override
+    public @NotNull String toString() {
         return major + "." + minor + "." + patch;
     }
 }
