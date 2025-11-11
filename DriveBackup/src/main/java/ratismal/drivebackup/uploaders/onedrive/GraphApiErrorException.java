a12278f8d78a10f5f63d426a35e915274bd75fa4
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2025-11-11 12:23:52 +0100

    remove incorrect GraphApiError parsing

--- DriveBackup/src/main/java/ratismal/drivebackup/uploaders/onedrive/GraphApiErrorException.java
+++ DriveBackup/src/main/java/ratismal/drivebackup/uploaders/onedrive/GraphApiErrorException.java
@@ -2,15 +2,10 @@ package ratismal.drivebackup.uploaders.onedrive;
 
 import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * an exception representing a microsoft graph api error
@@ -18,23 +13,16 @@ import java.util.stream.Collectors;
 public class GraphApiErrorException extends Exception {
     private static final String ERROR_OBJ_KEY = "error";
     private static final String CODE_STR_KEY = "code";
-    private static final String INNERERROR_OBJ_KEY = "innererror";
     private static final String MESSAGE_STR_KEY = "message";
-    private static final String DETAILS_ARR_KEY = "details";
 
-    /** status code of the response or -1 if not available */
+    /** status code of the response */
     public final int statusCode;
     /** an error code string for the error that occurred */
     public final String errorCode;
     /** a developer ready message about the error that occurred. this shouldn't be displayed to the user directly */
     public final String errorMessage;
-    /** optional list of additional error objects that might be more specific than the top-level error */
-    public final List<String> innerErrors;
-    /**
-     * optional list of additional error objects that might provide a breakdown of multiple errors encountered
-     * while processing the request
-     */
-    public final List<GraphApiErrorException> details;
+    /** the full error object */
+    public final JSONObject errorObject;
 
     /**
      * create the exception from a response
@@ -59,56 +47,20 @@ public class GraphApiErrorException extends Exception {
         this(statusCode, new JSONObject(responseBody).getJSONObject(ERROR_OBJ_KEY));
     }
 
-    private static List<String> parseInnerErrors(@Nullable JSONObject innerErrors) {
-        List<String> list = new ArrayList<>();
-        while (innerErrors != null) {
-            String errorCode = innerErrors.optString(CODE_STR_KEY);
-            if (errorCode != null) {
-                list.add(errorCode);
-            }
-            innerErrors = innerErrors.optJSONObject(INNERERROR_OBJ_KEY);
-        }
-        return list;
-    }
-
-    private static List<GraphApiErrorException> parseDetails(@Nullable JSONArray details) {
-        if (details == null) {
-            return new ArrayList<>();
-        }
-        List<GraphApiErrorException> list = new ArrayList<>(details.length());
-        for (int detailIdx = 0; detailIdx < details.length(); detailIdx++) {
-            list.add(new GraphApiErrorException(-1, details.getJSONObject(detailIdx).getJSONObject(ERROR_OBJ_KEY)));
-        }
-        return list;
-    }
-
     private GraphApiErrorException(int statusCode, @NotNull JSONObject error) {
-        this(statusCode, error.getString(CODE_STR_KEY), error.getString(MESSAGE_STR_KEY),
-            parseInnerErrors(error.optJSONObject(INNERERROR_OBJ_KEY)),
-            parseDetails(error.optJSONArray(DETAILS_ARR_KEY)));
+        this(statusCode, error.getString(CODE_STR_KEY), error.getString(MESSAGE_STR_KEY), error);
     }
 
-    private static String toMessage(int statusCode, String errorCode, String errorMessage, List<String> innerErrors,
-        List<GraphApiErrorException> details) {
-        String format = "%d %s : \"%s\"%s%s";
-        String inner = String.join("\", \"", innerErrors);
-        String detail = details.stream().map(GraphApiErrorException::getMessage).collect(Collectors.joining(" }, { "));
-        if (!inner.isEmpty()) {
-            inner = " inner:[ \"" + inner + "\" ]";
-        }
-        if (!detail.isEmpty()) {
-            detail = " details:[ { " + detail + " } ]";
-        }
-        return String.format(format, statusCode, errorCode, errorMessage, inner, detail);
+    private static String toMessage(int statusCode, String errorCode, String errorMessage, JSONObject errorObject) {
+        String format = "%d %s : \"%s\"\n%s";
+        return String.format(format, statusCode, errorCode, errorMessage, errorObject.toString(2));
     }
 
-    private GraphApiErrorException(int statusCode, String errorCode, String errorMessage, List<String> innerErrors,
-        List<GraphApiErrorException> details) {
-        super(toMessage(statusCode, errorCode, errorMessage, innerErrors, details));
+    private GraphApiErrorException(int statusCode, String errorCode, String errorMessage, JSONObject errorObject) {
+        super(toMessage(statusCode, errorCode, errorMessage, errorObject));
         this.statusCode = statusCode;
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
-        this.innerErrors = innerErrors;
-        this.details = details;
+        this.errorObject = errorObject;
     }
 }
